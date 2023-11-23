a4a7dc324ccd224a40f5f0b2fd919b1a6c67d40a
BielSaki <gamersdeminecraft7@gmail.com>
2023-11-23 17:28:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ def main():
  x = input("Me diga um número em °C:")
  try:
   xx = int( x )
-  print("sua temperatura em °C é", x)
+  print("sua temperatura em °C é {}" .format(x), end=', ')
   y = 273
   yy = int( y )
   z = 1.8 * xx
   zz = float( z )
-  print("sua temperatura em °K é", xx + yy)
-  print("sua temperatura em F é", zz + 32)
+  print("sua temperatura em °K é {}" .format(xx+yy), end=', ')
+  print("sua temperatura em F é {}." .format(zz+32))
  except (ValueError): print("Por favor, insira apenas números!")
 
 main()
